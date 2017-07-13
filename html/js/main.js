71bfeb681e306156ee639ee9b96daf8a95014635
StevenViquez <sviquez48@gmail.com>
2017-07-12 22:03:17 -0600

    Add files via upload
    Iniciando proyecto falta incluir varios cambios a "servicios"

---
+++ html/js/main.js
@@ -0,0 +1,9 @@
+var menu = document.getElementById('menu');
+var altura = menu.offsetTop;
+window.addEventListener('scroll', function () {
+if (window.pageYOffset > altura) {
+    menu.classList.add('fixed');
+} else {
+    menu.classList.remove('fixed') 
+}
+})
